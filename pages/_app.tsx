5bd33c42f43a3645b754c74cd19beeb4a2d88e00
Yoriki Yamaguchi <yorikiyamaguchi@gmail.com>
2022-01-27 04:03:22 +0900

    json2yaml

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,9 @@
 import "../styles/globals.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
 import type { AppProps } from "next/app";
 import CssBaseline from "@mui/material/CssBaseline";
 import Box from "@mui/material/Box";
