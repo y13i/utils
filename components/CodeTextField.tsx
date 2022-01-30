053c54bc95eed0ef634960f000af43e46bf0dad5
Yoriki Yamaguchi <yorikiyamaguchi@gmail.com>
2022-01-31 04:05:53 +0900

    Update CodeTextField.tsx

--- components/CodeTextField.tsx
+++ components/CodeTextField.tsx
@@ -1,4 +1,5 @@
 import { useState, VFC } from "react";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import TextField, { TextFieldProps } from "@mui/material/TextField";
 import InputAdornment from "@mui/material/InputAdornment";
 import IconButton from "@mui/material/IconButton";
@@ -11,38 +12,52 @@ export const CodeTextField: VFC<TextFieldProps> = (props) => {
   >("Copy to clipboard");
 
   return (
-    <TextField
-      fullWidth
-      sx={{ my: 1 }}
-      InputProps={{
-        sx: { fontFamily: "monospace" },
-        endAdornment: (
-          <InputAdornment
-            position="end"
-            sx={props.multiline ? { alignSelf: "flex-start", mt: 1 } : {}}
-          >
-            <Tooltip
-              title={copyMessage}
-              onClose={() =>
-                setTimeout(() => setCopyMessage("Copy to clipboard"), 250)
-              }
+    <ThemeProvider
+      theme={createTheme({
+        palette: {
+          text: {
+            disabled: "rgba(0, 0, 0, 0.7)",
+          },
+        },
+      })}
+    >
+      <TextField
+        fullWidth
+        sx={{
+          my: 1,
+        }}
+        InputProps={{
+          sx: {
+            fontFamily: "monospace",
+          },
+          endAdornment: (
+            <InputAdornment
+              position="end"
+              sx={props.multiline ? { alignSelf: "flex-start", mt: 1 } : {}}
             >
-              <IconButton
-                aria-label={copyMessage}
-                onClick={() =>
-                  navigator.clipboard
-                    .writeText(props?.value as string)
-                    .then(() => setCopyMessage("Copied!"))
+              <Tooltip
+                title={copyMessage}
+                onClose={() =>
+                  setTimeout(() => setCopyMessage("Copy to clipboard"), 250)
                 }
-                edge="end"
               >
-                <ContentCopyIcon />
-              </IconButton>
-            </Tooltip>
-          </InputAdornment>
-        ),
-      }}
-      {...props}
-    />
+                <IconButton
+                  aria-label={copyMessage}
+                  onClick={() =>
+                    navigator.clipboard
+                      .writeText(props?.value as string)
+                      .then(() => setCopyMessage("Copied!"))
+                  }
+                  edge="end"
+                >
+                  <ContentCopyIcon />
+                </IconButton>
+              </Tooltip>
+            </InputAdornment>
+          ),
+        }}
+        {...props}
+      />
+    </ThemeProvider>
   );
 };
