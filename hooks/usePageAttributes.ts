c8dfb788cd8e0b3dff1ef2360460685f515a55dd
Yoriki Yamaguchi <yorikiyamaguchi@gmail.com>
2023-01-19 17:22:32 +0900

    implement jwt viewer

--- hooks/usePageAttributes.ts
+++ hooks/usePageAttributes.ts
@@ -2,6 +2,7 @@ import { ReactNode } from "react";
 
 import { pageAttribute as jsonYaml } from "../pages/json-yaml";
 import { pageAttribute as encodeDecode } from "../pages/encode-decode";
+import { pageAttribute as jwt } from "../pages/jwt";
 import { pageAttribute as myip } from "../pages/myip";
 import { pageAttribute as password } from "../pages/password";
 import { pageAttribute as qrcode } from "../pages/qrcode";
@@ -16,5 +17,5 @@ export type PageAttribute = {
 };
 
 export function usePageAttributes(): PageAttribute[] {
-  return [jsonYaml, encodeDecode, myip, password, qrcode, uuid, decoji];
+  return [jsonYaml, encodeDecode, jwt, myip, password, qrcode, uuid, decoji];
 }
