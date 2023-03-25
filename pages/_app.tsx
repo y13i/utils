e442ebab4668ab85462a4963a46f1841fdc45ca5
Yoriki Yamaguchi <yorikiyamaguchi@gmail.com>
2023-03-26 00:51:49 +0900

    Replace react-query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import type { AppProps } from "next/app";
-import { QueryClient, QueryClientProvider } from "react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
--- pages/myip.tsx
+++ pages/myip.tsx
@@ -1,5 +1,5 @@
 import { NextPage } from "next";
-import { useQuery } from "react-query";
+import { useQuery } from "@tanstack/react-query";
 import ky from "ky";
 
 import Button from "@mui/material/Button";
